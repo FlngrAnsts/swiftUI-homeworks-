0db42c85194522e9387c9e346eb1ac5c7a6da9cd
Anastasiya Felinger <felinger.nastya@gmail.com>
2024-11-21 17:12:19 +0300

    Initial Commit

---
+++ SwiftUI-Homework/SwiftUI_HomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_HomeworkApp.swift
+//  SwiftUI-Homework
+//
+//  Created by Anastasiya on 21.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_HomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
